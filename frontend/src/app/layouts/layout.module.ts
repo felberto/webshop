6327d83d69949641853d7bab2037388af5a65d73
felberto <tobias.felber97@gmail.com>
2019-02-10 19:07:03 +0100

    changed navbar item to float right

--- frontend/src/app/layouts/layout.module.ts
+++ frontend/src/app/layouts/layout.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from "@angular/core";
 import {NavigationComponent} from "./navigation/navigation.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     NavigationComponent
   ],
   exports: [NavigationComponent],
-  imports: [],
+  imports: [CommonModule],
   providers: []
 })
 export class LayoutModule {
