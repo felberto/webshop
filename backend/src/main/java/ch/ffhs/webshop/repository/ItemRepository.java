c9a0dd95772cdb4d58ed4fbf30c3e192005d2120
Tobias Felber <38561858+felberto@users.noreply.github.com>
2019-02-15 16:52:32 +0100

    missing annotation
    Co-Authored-By: Meli-01 <47660505+Meli-01@users.noreply.github.com>

--- backend/src/main/java/ch/ffhs/webshop/repository/ItemRepository.java
+++ backend/src/main/java/ch/ffhs/webshop/repository/ItemRepository.java
@@ -3,6 +3,7 @@ package ch.ffhs.webshop.repository;
 import ch.ffhs.webshop.domain.Item;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface ItemRepository extends CrudRepository<Item, Long> {
 
 }
