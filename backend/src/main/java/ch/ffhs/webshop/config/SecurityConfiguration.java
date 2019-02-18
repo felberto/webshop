91b6bae81d6cc3b7294e76740eddf0b5da41049b
felberto <tobias.felber97@gmail.com>
2019-02-18 15:25:27 +0100

    fixed issues

--- backend/src/main/java/ch/ffhs/webshop/config/SecurityConfiguration.java
+++ backend/src/main/java/ch/ffhs/webshop/config/SecurityConfiguration.java
@@ -12,25 +12,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ *
+ * @author Tobias Felber
+ * @author Melanie Ockenfels
+ */
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .cors();
+        http.csrf().disable();
+        http.cors();
         http.authorizeRequests().antMatchers("/**").permitAll();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+        final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
