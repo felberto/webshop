d5d1acf0e420e4f19ea1ca5c8a4a67c4c9209d33
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2019-04-02 23:50:55 +0200

    changed profile form and validator to use ngModel

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {Customer} from "../models/customer";
 import {AuthenticationService} from "../services/authentication.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {first} from "rxjs/operators";
 import {CustomerService} from "../services/customer.service";
 import {ToastrService} from "ngx-toastr";
@@ -13,10 +12,8 @@ import {ToastrService} from "ngx-toastr";
 export class ProfileComponent implements OnInit {
 
   currentUser: Customer;
-  profileForm: FormGroup;
-  submitted = false;
 
-  constructor(private authService: AuthenticationService, private formBuilder: FormBuilder, private customerService: CustomerService,
+  constructor(private authService: AuthenticationService, private customerService: CustomerService,
               private toastr: ToastrService) {
     this.authService.currentUser.subscribe(x => this.currentUser = x);
   }
@@ -25,26 +22,15 @@ export class ProfileComponent implements OnInit {
     this.customerService.getProfile(this.currentUser.id).subscribe(res => {
       this.currentUser = res.body;
     });
-    this.profileForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.pattern("[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}")]]
-    });
   }
 
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.profileForm.controls;
-  }
   onSubmit(){
-    this.submitted = true;
-
     // stop here if form is invalid
-    if (this.profileForm.invalid) {
-      return;
-    }
-
-    this.customerService.update(this.currentUser.id, this.profileForm.value)
+    // if (this.profileForm.invalid) {
+    //   return;
+    // }
+    // Todo: update function
+    this.customerService.update(this.currentUser.id, this.currentUser)
       .pipe(first())
       .subscribe(
         data => {
